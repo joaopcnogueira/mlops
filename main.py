19049e7f381d1a5e38ea4be366c27af0bdbbc6b0
João Paulo Nogueira <joaonogueira@fisica.ufc.br>
2021-05-02 09:40:52 -0300

    House Prices API adicionada

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from flask import Flask
+import joblib
+from flask import Flask, request, jsonify
 from textblob import TextBlob
 
 app = Flask(__name__)
+
+# loading the model object to predict house prices
+model_obj = joblib.load('models/model_prices.pkl')
+model = model_obj['model']
+features = model_obj['features']
+
 
 @app.route('/')
 def home():
@@ -17,5 +24,14 @@ def sentimento(frase):
     return f"""INPUT: {str(tb)};
                INPUT (ENG): {str(tb_en)};
                POLARIDADE: {polaridade}"""
+
+
+@app.route('/cotacao/', methods=['GET', 'POST'])
+def cotacao():
+    dados = request.get_json()
+    dados_input = [dados[col] for col in features]
+    preco = model.predict([dados_input])
+    return jsonify(preco=preco[0])
+
 
 app.run(debug=True)
