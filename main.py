c5196cd81ae0719b795c54ee8578f0dcddedbc06
João Paulo Nogueira <joaonogueira@fisica.ufc.br>
2021-05-01 22:36:45 -0300

    Sentiment Analysis Endpoint

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from flask import Flask
+from textblob import TextBlob
 
-app = Flask('meu_app')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return 'Hello World'
 
-@app.route('/predict')
-def predict():
-    return 'Sooner will get a model here!'
 
-app.run()
+@app.route('/sentimento/<frase>') # passando a frase na url
+def sentimento(frase):
+    tb = TextBlob(frase)
+    tb_en = tb.translate(to='en')
+    polaridade = tb_en.sentiment.polarity
+
+    return f"""INPUT: {str(tb)};
+               INPUT (ENG): {str(tb_en)};
+               POLARIDADE: {polaridade}"""
+
+app.run(debug=True)
